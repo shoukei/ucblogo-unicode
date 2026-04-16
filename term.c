840d9fb5e26186e40e883425800aaaee84db0419
shoukei <shoukei3@yahoo.co.jp>
2026-04-16 11:13:28 +0900

    Use tcsetattr instead of ioctl

--- term.c
+++ term.c
@@ -125,7 +125,8 @@ void term_init(void) {
 
     if (interactive) {
 #if defined(HAVE_TERMIO_H) || defined(HAVE_TERMIOS_H)
-	ioctl(0,TCGETA,(char *)(&tty_cooked));
+	/* ioctl(0,TCGETA,(char *)(&tty_cooked)); */
+	tcgetattr(0, &tty_cooked);
 	tty_cbreak = tty_cooked;
 	tty_cbreak.c_cc[VMIN] = '\01';
 	tty_cbreak.c_cc[VTIME] = '\0';
@@ -185,7 +186,8 @@ void charmode_on() {
 #ifdef unix
     if ((readstream == stdin) && interactive && !tty_charmode) {
 #if defined(HAVE_TERMIO_H) || defined(HAVE_TERMIOS_H)
-	ioctl(0,TCSETA,(char *)(&tty_cbreak));
+	/* ioctl(0,TCSETA,(char *)(&tty_cbreak)); */
+	tcsetattr(0, TCSANOW, &tty_cbreak);
 #else /* !HAVE_TERMIO_H */
 	ioctl(0,TIOCSETP,(char *)(&tty_cbreak));
 #endif /* HAVE_TERMIO_H */
@@ -201,7 +203,8 @@ void charmode_off() {
 #ifdef unix
     if (tty_charmode) {
 #if defined(HAVE_TERMIO_H) || defined(HAVE_TERMIOS_H)
-	ioctl(0,TCSETA,(char *)(&tty_cooked));
+	/* ioctl(0,TCSETA,(char *)(&tty_cooked)); */
+	tcsetattr(0, TCSANOW, &tty_cooked);
 #else /* !HAVE_TERMIO_H */
 	ioctl(0,TIOCSETP,(char *)(&tty_cooked));
 #endif /* HAVE_TERMIO_H */
